fe149cd33485854c9a6c4f8b611adc3e134f3883
Yan Cheng Cheok <yccheok@yahoo.com>
2015-10-27 08:11:47 +0800

    Change action bar title & remove back button, when in inner preference screen.

--- app/src/main/java/com/madlymad/preferenceapp/MyInnerPreferenceFragment.java
+++ app/src/main/java/com/madlymad/preferenceapp/MyInnerPreferenceFragment.java
@@ -1,6 +1,7 @@
 package com.madlymad.preferenceapp;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * A simple Preference Fragment that shows the xml on Demand.
@@ -10,6 +11,13 @@ public class MyInnerPreferenceFragment extends AppPreferenceFragment {
     public static final String FRAGMENT_TAG = "my_inner_preference_fragment";
 
     public MyInnerPreferenceFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Inner fragment");
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 
     @Override
